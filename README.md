e7ef59bc1578a7d8a540b8aaf04e97e7919f6747
Luke Paireepinart <lukepdev@live.com>
2020-05-05 18:36:31 -0500

    - Fix bugs with firing not working correctly in some cases (eg. case insensitive matching) - Implement max lookback for optimization of handling

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # quill-fire
+
 Extend Quill JS editor to fire events as soon as matching text is entered!
+
+# Example usage:
+
+```javascript
+const fire: FireOptions = {
+  items: [
+    {
+      action: () => {
+        console.log("Found!");
+        return null;
+      },
+      matchString: "/",
+      prefix: /([\s.?!)]+$)|(^$)/,
+      ignoreCase: true,
+    },
+    {
+      action: () => {
+        console.log("Found case insensitive string: MATCH");
+        return null;
+      },
+      matchString: "MATCH",
+      ignoreCase: true,
+      prefix: /\s+$/,
+      removeMatchingText: true,
+    },
+    {
+      action: () => {
+        console.log("Found case sensitive string: caSeTest");
+        return null;
+      },
+      matchString: "caSeTest",
+      removeMatchingText: false,
+      ignoreCase: false,
+    },
+    {
+      action: () => {
+        console.log("Found exact string Bar after string FOO");
+        return null;
+      },
+      prefix: /FOO$/,
+      matchString: "Bar",
+      removeMatchingText: false,
+      ignoreCase: false,
+    },
+  ],
+};
+```
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,31 @@
-import Quill from "quill";
-import { SelectionChangeHandler, TextChangeHandler } from "quill";
+/* eslint-disable operator-linebreak */
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+// eslint-disable-next-line no-unused-vars
+import Quill, { TextChangeHandler } from "quill";
+
+type FireAction = () => string | void;
 
 export interface FireItem {
   matchString: string;
-  action: () => string | null; // Calls action.  TODO: support returning a Delta to build mention functionality. If string is returned, adds string to contents.
+  action: FireAction; // Action to be invoked.
+  // TODO: support returning a Delta to build mention functionality.
+  // If string is returned, adds string to contents.
   ignoreCase?: boolean;
-  removeMatchingText?: boolean; // if false, will leave matching text but will fire, if true, will delete matching text before firing.
-  prefix?: RegExp; // if a matchString is found, ensures the given prefix regex matches before the string before firing.
+  removeMatchingText?: boolean; // if false, will leave matching text but will fire.
+  //  if true, will delete matching text before firing.
+  prefix?: RegExp; // if a matchString is found,
+  // ensures the given prefix regex matches before the string before firing.
+  maxPrefixLookback?: number;
 }
 
 interface FireInternalItem {
   matchString: string;
-  action: () => string | null; // Calls action.  TODO: support returning a Delta to build mention functionality. If string is returned, adds string to contents.
+  action: FireAction;
   ignoreCase: boolean;
-  removeMatchingText: boolean; // if false, will leave matching text but will fire, if true, will delete matching text before firing.
-  prefix: RegExp; // if a matchString is found, ensures the given prefix regex matches before the string before firing.
+  removeMatchingText: boolean;
+  prefix?: RegExp;
+  maxPrefixLookback: number;
   matchLength: number;
 }
 
@@ -29,7 +40,9 @@ interface FireInternalOptions {
 
 class Fire {
   private quill: Quill;
+
   private options: FireInternalOptions;
+
   private longestItem: number;
 
   equalsIgnoringCase(text: string, other: string) {
@@ -52,8 +65,9 @@ class Fire {
         // Set default parameters on options if they are not set.
         ignoreCase: item.ignoreCase ?? true,
         removeMatchingText: item.removeMatchingText ?? true,
-        prefix: item.prefix ?? /([\s.?!)]+$)|(^$)/,
+        prefix: item.prefix ?? undefined,
         matchLength: item.matchString.length,
+        maxPrefixLookback: item.maxPrefixLookback ?? 1, // Only look at the immediately preceding character unless a larger match is requested.
       }));
     }
 
@@ -65,30 +79,34 @@ class Fire {
     // quill.on("selection-change", this.selectionChanged.bind(this));
   }
 
-  selectionChanged: SelectionChangeHandler = (range, oldRange, source) => {};
+  //   selectionChanged: SelectionChangeHandler = (range, oldRange, source) => {};
 
   textChanged: TextChangeHandler = (delta, oldContents, source) => {
+    // console.log(source);
+    // console.log(this.quill.getText());
     // If the text change did not come from the user, then break out
     if (source !== "user") return;
 
-    // Store the full preceding text so we can do the prefix regex match, but don't retrieve it until the first time it is used.
-    var fullPrefixText: string | null = null;
+    // If there is a currently selected range or the editor doesn't have focus (range is null),
+    // break out of this method.
+    const range = this.quill.getSelection();
+    if (range && range.length !== 0) return;
 
-    // If there is a currently selected range or the editor doesn't have focus (range is null), break out of this method.
-    var range = this.quill.getSelection();
-    if (!range || range.length !== 0) return;
-
-    // range.index is the currently selected position. Try and get our longestItem but if that's not possible, get the longest pre match we can get.
-    const end = range.index;
+    // range.index is the currently selected position.
+    // Try and get our longestItem but if that's not possible, get the longest pre match we can get.
+    const end = range?.index ?? Math.max(0, this.quill.getLength() - 1);
     const start = Math.max(0, end - this.longestItem);
     const candidateText = this.quill.getText(start, end - start);
-    for (const item of this.options.items) {
-      // If the target match is longer than the substring we were able to get, there's no way it could fire.
+    // console.log(`candidate text: ${candidateText}`);
+    this.options.items.some((item) => {
+      // If the target match is longer than the substring we were able to get,
+      // there's no way it could fire.
       if (candidateText.length < item.matchLength) {
-        continue;
+        return false;
       }
-      // If we're here then candidateText is either the same length or longer than our test, so let's get an exact length starting at the end.
-      let candidateTextItemSpecific = candidateText.substring(
+      // If we're here then candidateText is either the same length or longer than our test,
+      // so let's get an exact length starting at the end.
+      const candidateTextItemSpecific = candidateText.substring(
         candidateText.length - item.matchLength
       );
 
@@ -97,43 +115,62 @@ class Fire {
         item.ignoreCase &&
         !this.equalsIgnoringCase(candidateTextItemSpecific, item.matchString)
       ) {
-        continue;
-      } else if (candidateTextItemSpecific === item.matchString) {
-        continue;
+        return false;
       }
-
+      if (!item.ignoreCase && candidateTextItemSpecific !== item.matchString) {
+        return false;
+      }
       // If there's a prefix defined, check that it matches.
       if (item.prefix !== undefined) {
-        // If no previous match has attempted to get full prefix text, get it now.  We're doing this to memoize the access of the text
-        // so we don't get it each time a prefix regex comes up.
-        if (fullPrefixText == null) {
-          fullPrefixText = this.quill.getText(0, Math.max(0, start));
-        }
+        //   0      lookback  start    end
+        //   [preamble]|[prefix]|[match]|
+        // We want to skip preamble and only capture prefix.
+        const windowStart = Math.max(
+          0,
+          end - item.matchLength - item.maxPrefixLookback
+        );
+        const windowEnd = Math.max(0, end - item.matchLength - windowStart);
+        const prefixText = this.quill.getText(windowStart, windowEnd);
+        // console.log(`windowStart: ${windowStart}`);
+        // console.log(`windowEnd: ${windowEnd}`);
+        // console.log(`PREFIX: ${prefixText}`);
+        // console.log(`candidate text: ${candidateTextItemSpecific}`);
+        // console.log(`prefix regex: ${item.prefix}`);
 
-        if (!item.prefix.test(fullPrefixText!)) {
-          continue;
+        if (!item.prefix.test(prefixText!)) {
+          return false;
         }
       }
-      // If we got here, either there was no prefix match requested or we matched prefix, so now we just need to decide if we need to remove matching text
+      let currentIndex = end;
+      // If we got here, either there was no prefix match requested or we matched prefix,
+      // so now we just need to decide if we need to remove matching text
       // then fire the event then check if there was a replacement response to insert.
       if (item.removeMatchingText) {
         this.quill.deleteText(
           end - candidateTextItemSpecific.length,
-          end,
+          candidateTextItemSpecific.length,
           "api"
         );
+        currentIndex -= candidateTextItemSpecific.length;
       }
-      var result = item.action();
-      if (result) {
-        this.quill.insertText(
-          end - candidateTextItemSpecific.length,
-          result,
-          "api"
-        );
-      }
+
+      //   const result = item.action();
+      //   console.log(result);
+      //   if (result !== undefined) {
+      //     this.quill.insertText(end, result, 'api');
+      //     currentIndex += result.length;
+      //   }
+
+      //   this.quill.setSelection(
+      //     {
+      //       index: 3,
+      //       length: 1,
+      //     },
+      //     'api',
+      //   );
       // Only match 1 match and stop looking if we got one.
-      break;
-    }
+      return true;
+    });
   };
 }
 
